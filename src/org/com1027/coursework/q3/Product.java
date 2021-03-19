84cbd472cc623063ae13b30da40f6dea89603c9c
shizzle8 <liibaan_1998@hotmail.com>
2021-03-19 21:00:57 +0000

    Restructuring the User and Auction class to allow the User to display their history pruchases and bids

---
+++ src/org/com1027/coursework/q3/Product.java
@@ -0,0 +1,41 @@
+package org.com1027.coursework.q3;
+
+import java.util.ArrayList;
+
+public abstract class  Product {
+  // all the fields I need to create a product
+  private int productID;
+  private String productName;
+
+  
+  // constructor allows a product instance to be created if it meets the requirements
+  public Product(int ID, String productName) {
+    if(productName==null) {
+      throw new IllegalArgumentException();
+    }
+    else {
+      this.productID=ID;
+      this.productName=productName;
+    }
+  }
+  
+  
+  // this method just returns the product ID.
+  public int getProductId() {
+    return productID;
+  }
+  // this method just returns the product name.
+  public String getProductName() {
+    return productName;
+  }
+
+  public String toString() {
+    return getProductId() + " - " + getProductName();
+  }
+  
+  public abstract String displayHistory();
+  public abstract String displayUserInfoForProduct();
+  public abstract double getCurrentPrice();
+  public abstract boolean isProductSold();
+  
+}
